fix: correct xyxy to tlwh calculation

pkd-jde-node/src/custom_nodes/dabble/mot_evaluator.py
@@ -68,9 +68,11 @@ class Node(AbstractNode):
                 return {}
             tlwhs = xyxyn2tlwh(inputs["bboxes"], *inputs["frame_size"])
             for tlwh, track_id in zip(tlwhs, inputs["obj_track_ids"]):
+                if int(track_id) < 0:
+                    continue
                 self.results.append(
                     f"{inputs['frame_idx']},{track_id},"
-                    f"{','.join(np.char.mod('%f', tlwh))},-1,-1,-1,-1\n"
+                    f"{','.join(np.char.mod('%f', tlwh))},1,-1,-1,-1\n"
                 )
 
         # print(inputs["seq_dir"], inputs["frame_idx"])

pkd-jde-node/src/custom_nodes/model/jdev1/jde_files/utils.py
@@ -437,24 +437,24 @@ def xywh2xyxy(x):
     return y
 
 
-# def xyxy2xyxyn(inputs: np.ndarray, height: float, width: float) -> np.ndarray:
-#     """Converts from [x1, y1, x2, y2] to normalised [x1, y1, x2, y2].
-#     (x1, y1) is the top left corner and (x2, y2) is the bottom right corner.
-#     Normalised coordinates are w.r.t. original image size.
-#     """
-#     outputs = np.empty_like(inputs)
-#     outputs[:, [0, 2]] = inputs[:, [0, 2]] / width
-#     outputs[:, [1, 3]] = inputs[:, [1, 3]] / height
+def xyxyn2xyxy(inputs: np.ndarray, height: float, width: float) -> np.ndarray:
+    """Converts from [x1, y1, x2, y2] to normalised [x1, y1, x2, y2].
+    (x1, y1) is the top left corner and (x2, y2) is the bottom right corner.
+    Normalised coordinates are w.r.t. original image size.
+    """
+    outputs = np.empty_like(inputs)
+    outputs[:, [0, 2]] = inputs[:, [0, 2]] * width
+    outputs[:, [1, 3]] = inputs[:, [1, 3]] * height
 
-#     return outputs
+    return outputs
 
 
 def xyxyn2tlwh(x, height, width):
     y = np.empty_like(x)
     y[:, 0] = x[:, 0] * width  # Bottom left x
     y[:, 1] = x[:, 1] * height  # Bottom left y
-    y[:, 2] = (x[:, 0] + x[:, 2]) * width  # Top right x
-    y[:, 3] = (x[:, 1] + x[:, 3]) * height  # Top right y
+    y[:, 2] = (x[:, 2] - x[:, 0]) * width  # Top right x
+    y[:, 3] = (x[:, 3] - x[:, 1]) * height  # Top right y
     return y
 
 