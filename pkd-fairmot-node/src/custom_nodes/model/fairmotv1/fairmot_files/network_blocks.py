86f69f5a1580df13f8d1c946de6dba271094310e
yier <56420072+liyier90@users.noreply.github.com>
2022-02-10 17:21:23 +0800

    refactor: add docstrings to network_blocks.py

--- pkd-fairmot-node/src/custom_nodes/model/fairmotv1/fairmot_files/network_blocks.py
+++ pkd-fairmot-node/src/custom_nodes/model/fairmotv1/fairmot_files/network_blocks.py
@@ -1,3 +1,10 @@
+"""Modifications:
+- Rename planes to channels to be consistent to Conv2d argument naming
+- Rearrange argument order in Tree
+"""
+
+from typing import List
+
 import torch
 from torch import nn
 
@@ -7,36 +14,57 @@ BN_MOMENTUM = 0.1
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, stride=1, dilation=1):
+    """Basic residual block structure used by DLA.
+
+    Args:
+        in_channels (int): Number of channels in the input image.
+        out_channels (int): Number of channels produced by the block.
+        stride (int): Stride of the convolution. Default is 1.
+        dilation (int): Spacing between kernel elements. Default is 1.
+    """
+
+    def __init__(
+        self, in_channels: int, out_channels: int, stride: int = 1, dilation: int = 1
+    ) -> None:
         super().__init__()
         self.conv1 = nn.Conv2d(
-            inplanes,
-            planes,
+            in_channels,
+            out_channels,
             kernel_size=3,
             stride=stride,
             padding=dilation,
             bias=False,
             dilation=dilation,
         )
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
+        self.bn1 = nn.BatchNorm2d(out_channels, momentum=BN_MOMENTUM)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(
-            planes,
-            planes,
+            out_channels,
+            out_channels,
             kernel_size=3,
             stride=1,
             padding=dilation,
             bias=False,
             dilation=dilation,
         )
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
+        self.bn2 = nn.BatchNorm2d(out_channels, momentum=BN_MOMENTUM)
         self.stride = stride
 
-    def forward(self, x, residual=None):
-        if residual is None:
-            residual = x
+    def forward(
+        self, inputs: torch.Tensor, residual: torch.Tensor = None
+    ) -> torch.Tensor:
+        """Defines the computation performed at every call.
 
-        out = self.conv1(x)
+        Args:
+            inputs (torch.Tensor): Input from the previous layer.
+
+        Returns:
+            (torch.Tensor): The output tensor of the block.
+        """
+        if residual is None:
+            residual = inputs
+
+        out = self.conv1(inputs)
         out = self.bn1(out)
         out = self.relu(out)
 
@@ -50,14 +78,21 @@ class BasicBlock(nn.Module):
 
 
 class DeformConv(nn.Module):
-    def __init__(self, chi, cho):
+    """Performs deformable convolution followed by batch norm.
+
+    Args:
+        in_channels (int): Number of channels in the input image.
+        out_channels (int): Number of channels produced by the convolution.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.actf = nn.Sequential(
-            nn.BatchNorm2d(cho, momentum=BN_MOMENTUM), nn.ReLU(inplace=True)
+            nn.BatchNorm2d(out_channels, momentum=BN_MOMENTUM), nn.ReLU(inplace=True)
         )
         self.conv = DCNv2(
-            chi,
-            cho,
+            in_channels,
+            out_channels,
             kernel_size=(3, 3),
             stride=1,
             padding=1,
@@ -65,14 +100,34 @@ class DeformConv(nn.Module):
             deformable_groups=1,
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:  # pylint: disable=invalid-name
+        """Defines the computation performed at every call.
+
+        Args:
+            x (torch.Tensor): Input from the previous layer.
+
+        Returns:
+            (torch.Tensor): The output tensor of the block.
+        """
         x = self.conv(x)
         x = self.actf(x)
         return x
 
 
 class Root(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, residual):
+    """Root node in Hierarchical Deep Aggregation.
+
+    Args:
+        in_channels (int): Number of channels in the input image.
+        out_channels (int): Number of channels produced by the block.
+        kernel_size (int): Size of the convolving kernel, used for calculating
+            padding size only.
+        residual (bool): Flag to indicate if a residual layer should be used.
+    """
+
+    def __init__(
+        self, in_channels: int, out_channels: int, kernel_size: int, residual: bool
+    ) -> None:
         super().__init__()
         self.conv = nn.Conv2d(
             in_channels,
@@ -82,72 +137,100 @@ class Root(nn.Module):
             bias=False,
             padding=(kernel_size - 1) // 2,
         )
-        self.bn = nn.BatchNorm2d(out_channels, momentum=BN_MOMENTUM)
+        self.bn = nn.BatchNorm2d(  # pylint: disable=invalid-name
+            out_channels, momentum=BN_MOMENTUM
+        )
         self.relu = nn.ReLU(inplace=True)
         self.residual = residual
 
-    def forward(self, *x):
-        children = x
-        x = self.conv(torch.cat(x, 1))
-        x = self.bn(x)
-        if self.residual:
-            x += children[0]
-        x = self.relu(x)
+    def forward(self, inputs: List[torch.Tensor]) -> torch.Tensor:
+        """Defines the computation performed at every call.
 
-        return x
+        Args:
+            inputs (List[torch.Tensor]): Inputs from the previous layers.
+
+        Returns:
+            (torch.Tensor): The output tensor of the block.
+        """
+        children = inputs
+        out = self.conv(torch.cat(inputs, 1))
+        out = self.bn(out)
+        if self.residual:
+            out += children[0]
+        out = self.relu(out)
+
+        return out
 
 
 class Tree(nn.Module):
+    """Tree node used to construct the structure of Hierarchical Deep
+    Aggregation (HDA).
+
+    Args;
+        level (int): The stage number of this node in HDA.
+        block (BasicBlock): The type of residual block used.
+        in_channels (int): Number of channels in the input image.
+        out_channels (int): Number of channels produced by the block.
+        stride (int): Stride of the convolution.
+        level_root (bool): Flag to indicate is this is the root node for the
+            stage.
+        dilation (int): Spacing between kernel elements. Default is 1.
+        root_dim (int): Number of channels in the input image to the Root node.
+        root_kernel_size (int): Size of the convolving kernel in the Root node.
+        residual_root (bool): Flag to indicate if a residual layer should be
+            used in the Root node. Default is False.
+    """
+
     def __init__(
         self,
-        levels,
-        block,
-        in_channels,
-        out_channels,
-        stride=1,
-        level_root=False,
-        root_dim=0,
-        root_kernel_size=1,
-        dilation=1,
-        root_residual=False,
-    ):
+        level: int,
+        block: "BasicBlock",
+        in_channels: int,
+        out_channels: int,
+        stride: int = 1,
+        level_root: bool = False,
+        dilation: int = 1,
+        root_dim: int = 0,
+        root_kernel_size: int = 1,
+        root_residual: bool = False,
+    ) -> None:
         super().__init__()
         if root_dim == 0:
             root_dim = 2 * out_channels
         if level_root:
             root_dim += in_channels
-        if levels == 1:
+        if level == 1:
             self.tree1 = block(in_channels, out_channels, stride, dilation=dilation)
             self.tree2 = block(out_channels, out_channels, 1, dilation=dilation)
         else:
             self.tree1 = Tree(
-                levels - 1,
+                level - 1,
                 block,
                 in_channels,
                 out_channels,
                 stride,
+                dilation=dilation,
                 root_dim=0,
                 root_kernel_size=root_kernel_size,
-                dilation=dilation,
                 root_residual=root_residual,
             )
             self.tree2 = Tree(
-                levels - 1,
+                level - 1,
                 block,
                 out_channels,
                 out_channels,
+                dilation=dilation,
                 root_dim=root_dim + out_channels,
                 root_kernel_size=root_kernel_size,
-                dilation=dilation,
                 root_residual=root_residual,
             )
-        if levels == 1:
+        if level == 1:
             self.root = Root(root_dim, out_channels, root_kernel_size, root_residual)
         self.level_root = level_root
         self.root_dim = root_dim
         self.downsample = None
         self.project = None
-        self.levels = levels
+        self.levels = level
         if stride > 1:
             self.downsample = nn.MaxPool2d(stride, stride=stride)
         if in_channels != out_channels:
@@ -158,17 +241,34 @@ class Tree(nn.Module):
                 nn.BatchNorm2d(out_channels, momentum=BN_MOMENTUM),
             )
 
-    def forward(self, x, residual=None, children=None):
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        residual: torch.Tensor = None,
+        children: List[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """Defines the computation performed at every call.
+
+        Args:
+            inputs (torch.Tensor): Input from the previous layer.
+            residual (torch.Tensor): Residual from the connected node.
+            children (List[torch.Tensor]): Inputs from child nodes of the
+                current node.
+
+        Returns:
+            (torch.Tensor): The output tensor of the block.
+        """
         children = [] if children is None else children
-        bottom = self.downsample(x) if self.downsample else x
+        bottom = self.downsample(inputs) if self.downsample else inputs
         residual = self.project(bottom) if self.project else bottom
         if self.level_root:
             children.append(bottom)
-        x1 = self.tree1(x, residual)
+        out_1 = self.tree1(inputs, residual)
         if self.levels == 1:
-            x2 = self.tree2(x1)
-            x = self.root(x2, x1, *children)
+            out_2 = self.tree2(out_1)
+            out = self.root([out_2, out_1] + children)
         else:
-            children.append(x1)
-            x = self.tree2(x1, children=children)
-        return x
+            children.append(out_1)
+            out = self.tree2(out_1, children=children)
+
+        return out
